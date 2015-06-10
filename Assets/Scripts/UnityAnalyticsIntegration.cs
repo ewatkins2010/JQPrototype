371d4de04d34b15b2b63780900fe3a983de955fc
Ssteve85uk <stephen.mangold@knights.ucf.edu>
2015-06-10 00:07:33 -0400

    cloud
    c

---
+++ Assets/Scripts/UnityAnalyticsIntegration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Cloud.Analytics;
+
+public class UnityAnalyticsIntegration : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+		const string projectId = "92309dbc-58d8-49d0-be7f-d43352ace097";
+		UnityAnalytics.StartSDK (projectId);
+		
+	}
+	
+}
